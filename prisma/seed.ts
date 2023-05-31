81b54245e2c05e7ddf5a2b2f629e2ffb995505e0
Preduh <pedro.lucas5860@gmail.com>
2023-05-30 22:13:05 -0300

    fix: validate user email when create the default user in seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,16 +1,24 @@
 import { prismaClient } from '../src/database/prismaClient'
 
 async function main (): Promise<void> {
-  await prismaClient.user.create({
-    data: {
-      email: process.env.DEFAULT_USER_EMAIL ?? 'dev@mail.com',
-      firstName: 'Pedro',
-      lastName: 'Lucas',
-      mobile: '38999999999',
-      password: process.env.DEFAULT_USER_PASSWORD ?? '123456',
-      role: 'admin'
+  const userFind = prismaClient.user.findFirst({
+    where: {
+      email: process.env.DEFAULT_USER_EMAIL ?? 'dev@mail.com'
     }
   })
+
+  if (userFind === null) {
+    await prismaClient.user.create({
+      data: {
+        email: process.env.DEFAULT_USER_EMAIL ?? 'dev@mail.com',
+        firstName: 'Pedro',
+        lastName: 'Lucas',
+        mobile: '38999999999',
+        password: process.env.DEFAULT_USER_PASSWORD ?? '123456',
+        role: 'admin'
+      }
+    })
+  }
 }
 
 main()
@@ -18,6 +26,7 @@ main()
     await prismaClient.$disconnect()
   })
   .catch(async (e) => {
+    console.log(e)
     await prismaClient.$disconnect()
     process.exit(1)
   })
